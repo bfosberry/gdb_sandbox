d3bc57ba17a2b2ddd4a788c99b12b927d9800594
Brendan Fosberry <brendan.fosberry@gmail.com>
2015-07-22 08:43:02 -0500

    Adding simple complications

--- main.go
+++ main.go
@@ -4,9 +4,31 @@ import (
 	"fmt"
 )
 
+type pair struct {
+	x int
+	y int
+}
+
 func main() {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
+		p := handleNumber(i)
+		fmt.Printf("%+v\n", p)
 		fmt.Println("looping")
 	}
 	fmt.Println("Done")
+}
+
+func handleNumber(i int) *pair {
+	val := i
+	if i%2 == 0 {
+		val = f(i)
+	}
+	return &pair{
+		x: i,
+		y: val,
+	}
+}
+
+func f(x int) int {
+	return x*x + x
 }
